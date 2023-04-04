b65b245a47e3f8cf6e4859db2482b8c083175d2c
Clement Pellerin <clement_pellerin@ibi.com>
2023-04-04 15:44:46 -0400

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# P3Deps
-P3Deps analyzes the dependencies in a P3 Unity project
+## P3Deps
+
+[P3Deps](https://github.com/clempo2/P3Deps) is a Perl script that analyzes the dependencies in a P3 Unity project and outputs a report of missing and unused assets.
+
+The [P3 SDK](https://www.multimorphic.com/support/projects/customer-support/wiki/3rd-Party_Development_Kit) is a development kit distributed by [Multimorphic](https://www.multimorphic.com/) to help the development of games on the [P3 Pinball platform](https://www.multimorphic.com/p3-pinball-platform/).
+
+A P3 Unity project is an application written in [Unity](https://unity.com/) using the P3 SDK.
+
+P3Deps was developped against P3_SDK_V0.8 primarily to trim down P3SampleApp. P3Deps is likely to be useful with any P3 project since almost all P3 projects can be traced back to P3SampleApp originally.
+
+## Status
+
+The script is released and continues to be maintained. Please submit a [GitHub issue](https://github.com/clempo2/P3Deps/issues) if you find a problem.
+
+## Installation
+
+P3Deps assumes the development platform is Windows.
+
+Prerequisite: P3Deps requires Perl 5 be installed on the PC.
+
+Perl comes bundled with [Git for Windows](https://git-scm.com/download/win). If you have Git for Windows installed, Perl is already available. Just make sure to add C:\Program Files\Git\usr\bin to your Windows PATH.
+
+If you don't have Git for Windows and don't want to install it, consider installing [ActivePerl](https://www.activestate.com/products/perl/) instead.
+
+To install P3Deps:
+- Just copy p3deps.pl anywhere on your PC.
+
+## Instructions
+
+P3Deps does not modify the P3 Unity project in any way. The only thing it does is print out a report on standard output.
+
+In order for P3Deps to read the assets, the Unity serialization must be changed to YAML. This can be done on a copy of the project. It does not have to be a permanent change.
+
+- Start Unity and open the P3 project.
+- In the top menu, select Edit/Project Settings/Editor.
+- In the Inspector, Change Asset Serialiazation Mode to Force Text.
+- Wait for the assets to be rewritten.
+
+- In a command prompt, type the following command:  perl path_to_p3deps absolute_path_to_project  
+  where path_to_p3deps is the path to the p3deps.pl file  
+  and absolute_path_to_project is the absolute path to the project directory.
+  
+  For example: perl c:/p3deps.pl c:/P3_SDK_V0.8/P3SampleApp > report.txt
+
+  Perl can get confused with relative paths. Make sure you use an absolute path to the project.  
+  We recommend forward slashes instead of backslashes to make the output work better.  
+- Revert the Serialization Mode if desired.
+
+P3Deps will emit a report with the following information:
+- A tree of the dependencies among used assets starting from every root
+- Missing resources that are referenced but could not be found in the project
+- A sample script to delete all the unused assets
+
+The next step is best performed on a copy of your project or after all changes have been committed to git.
+
+If you choose to trim your project:
+- Extract the rm and rmdir commands from the output and write them in new a batch file like rmdeps.bat
+- Verify carefully that all these files and directories are indeed not needed.
+- Add C:\Program Files\Git\usr\bin to your PATH if not already there.
+- Execute: rmdeps.bat
+
+## Theory of Operations
+
+P3Deps starts with a list of assets it knows are used in the project:
+- the enabled scenes in the project build settings
+- Assets/Plugins
+- Assets/Editor
+- Assets/Gizmos
+- Assets/Resources/Prefabs/Framework
+- Assets/Scripts/GUI/${AppCode}Setup.cs
+- A few resources loaded by the SDK
+
+P3Deps traverses the assets starting from these roots and follows references to other assets. For scripts, P3Deps finds referenced classes, calls to Resources.Load(), calls to PlaySound() and PlaySound3D().
+
+## License
+
+Copyright (c) 2023 Clement Pellerin  
+MIT License.
---
+++ p3deps.pl
@@ -0,0 +1,448 @@
+# P3Deps - Analyze dependencies in a P3 Unity project
+# Copyright (c) 2023 Clement Pellerin  
+# MIT License.
+
+use strict;
+use warnings;
+use File::Find;
+
+if (@ARGV < 1) {
+  print "Usage: perl p3deps.pl <project>\n";
+  print "where project is the absolute path to the P3 Unity project\n";
+  exit;
+}
+
+my $project = $ARGV[0];
+my $assets = "$project/Assets";
+my $appconfig = "$project/Configuration/AppConfig.json";
+my $buildsettings = "$project/ProjectSettings/EditorBuildSettings.asset";
+
+if (!(-d "$assets" and -f "$appconfig" and -f $buildsettings)) {
+  print "The path '$project' is not a P3 Unity project\n";
+  exit;
+}
+
+# set of scene names in the project build settings
+my %scenes = ();
+
+# set of root assets that are known to be used
+my %roots = ();
+
+# maps resource path (without extension) to asset path (with extension)
+my %fullpaths = ();
+
+# set of directory paths under Assets
+my %dirs = ();
+
+# maps asset path to guid
+my %guids = ();
+
+# maps guid to asset path
+my %paths = ();
+
+# maps a class name to its file path
+my %classes = ();
+
+# maps script asset path to set of identifiers in the source
+my %identifiers = ();
+
+# maps asset path to set of guid references
+my %refs = ();
+
+# maps asset path to set of resources used
+my %resources = ();
+
+# maps asset path to set of dependency paths
+my %deps = ();
+
+# set of asset paths used by the project
+my %used = ();
+
+# set of resources used but missing
+my %missing = ();
+
+# find the scene names from the project build settings and mark them as roots
+find_scenes();
+
+# find all dirs, meta files and the asset guids
+find(\&process_file, $assets);
+
+# add some hardcoded roots
+mark_roots("$assets/Resources/Prefabs/Framework");
+mark_roots("$assets/Editor");
+mark_roots("$assets/Gizmos");
+mark_roots("$assets/Plugins/ConsoleE");
+mark_roots("$assets/Plugins/sqlite3.def");
+find(\&mark_dlls, "$assets/Plugins");
+delete $roots{"$assets/Plugins/mysql.data.dll"};
+
+my $appcode = find_appcode();
+my $appsetup = "$assets/Scripts/GUI/${appcode}Setup.cs";
+$roots{$appsetup} = 1;
+
+# add some resources loaded by the SDK, fake they are loaded by ${appcode}Setup.cs
+$resources{$appsetup}{"$assets/Resources/Fonts/tunga"} = 1;
+$resources{$appsetup}{"$assets/Resources/Fonts/sf distant galaxy alternate italic"} = 1;
+$resources{$appsetup}{"$assets/Resources/Prefabs/${appcode}Setup"} = 1;
+$resources{$appsetup}{"$assets/Resources/Prefabs/${appcode}NamedLocations"} = 1;
+$resources{$appsetup}{"$assets/Resources/Prefabs/${appcode}PopupScore"} = 1;
+$resources{$appsetup}{"$assets/Resources/Prefabs/GUI/TwitchChatBot"} = 1;
+$resources{$appsetup}{"$assets/Resources/Prefabs/GUI/PopupMessage"} = 1;
+
+# find all the assets used by the project by traversing from the roots
+foreach my $root (keys %roots) {
+  traverse($root, "");
+}
+
+print "\n\n\n\n =========== Used Assets ===========\n";
+foreach my $path (sort keys %used) {
+  print "$path\n";
+}
+
+print "\n\n\n\n =========== Missing Resources ===========\n";
+foreach my $path (sort keys %missing) {
+  print "$path\n";
+}
+
+print "\n\n\n\n =========== Unused Assets  ===========\n";
+my $standard_assets = "$assets/Standard Assets";
+foreach my $path (sort keys %guids) {
+  if (!$used{$path} and rindex($path, $standard_assets, 0) == -1) {
+    print "rm \"$path.meta\"\n";
+    print "rm \"$path\"\n";
+  }
+}
+
+foreach my $dir (reverse sort keys %dirs) {
+  if (!is_used_dir($dir) and rindex($dir, $standard_assets, 0) == -1) {
+    print "rm \"$dir.meta\"\n";
+    print "rmdir \"$dir\"\n";
+  }
+}
+
+if (-d "$project/Documentation") {
+  print "\n\nThe Documentation directory can also be removed from your project\n";
+}
+
+exit;
+
+# given a used asset, traverse its dependencies to mark them used
+sub traverse {
+  my $path = $_[0];
+  my $indent = $_[1];
+  if ($used{$path}) {
+    print "$indent$path [again]\n";
+  }
+  else {
+    $used{$path} = 1;
+    print "$indent$path\n";
+
+    my %refguids = %{$refs{$path}};
+    foreach my $refguid (keys %refguids) {
+      if (defined $paths{$refguid}) {
+        traverse($paths{$refguid}, $indent . "  ");
+      } 
+      #else {
+      #  print "$indent  Unknown $refguid\n";
+      #}
+    }
+
+    my %resourceset = %{$resources{$path}};
+    foreach my $resource (keys %resourceset) {
+      my $respath = resource_fullpath($resource);
+      if ($respath) {
+        traverse($respath, $indent . "  ");
+      }
+      else {
+        $missing{$resource} = 1;
+      }
+    }
+
+    if (defined $identifiers{$path}) {
+      my %identifierset = %{$identifiers{$path}};
+      foreach my $identifier (keys %identifierset) {
+        if ($classes{$identifier}) {
+          traverse($classes{$identifier}, $indent . "  ");
+	}
+      }
+    }
+  }
+}
+
+sub resource_fullpath {
+  my $resource = $_[0];
+  foreach my $path (keys %guids) {
+    return $path if rindex($path, $resource, 0) == 0;
+  }
+
+  # print "Unknown resource $resource\n";
+  return "";
+}
+
+sub show_all_refs {
+  print "Refs ======================\n";
+  foreach my $path (keys %guids) {
+    print "$path\n";
+    
+    my %guidset = %{$refs{$path}};
+    foreach my $guid (keys %guidset) {
+      if (defined $paths{$guid}) {
+        print "  $paths{$guid}\n";
+      } 
+      else {
+        print "  Unknown $guid\n";
+      }
+    }
+  }
+}
+
+sub show_all_resources {
+  print "Resources ======================\n";
+  foreach my $path (keys %guids) {
+    print "$path\n";
+    
+    my %resourceset = %{$resources{$path}};
+    foreach my $resource (keys %resourceset) {
+      print "  $resource\n";
+    }
+  }
+}
+
+sub process_file {
+  if (-f $_ && $_ =~ /\.meta$/) {
+    my $meta = $File::Find::name;
+    
+    my $path = $meta;
+    $path =~ s/\.meta$//;
+
+    my $respath = $path;
+    $respath =~ s/\.[^.]+$//;
+    $fullpaths{$respath} = $path;
+    $refs{$path} = {};
+    $deps{$path} = {};
+    $resources{$path} = {};
+
+    if (-d $path) {
+      $dirs{$path} = 1;
+    }
+    elsif (-f $path) {
+      my $guid = read_guid($meta);
+      $guids{$path} = $guid;
+      $paths{$guid} = $path;
+
+      if ($path =~ /\.cs$/) {
+        process_script($path);
+      }
+      elsif (!is_binary($path)) {
+        process_asset($path);
+      }
+    }
+  }
+}
+
+sub read_guid {
+  my $meta = $_[0];
+  my $contents = read_file($meta);
+  $contents =~ m/\bguid: (.*)/;
+  my $guid = $1;
+  $guid || die "Missing guid in meta file $meta";
+}
+
+sub process_script {
+  my $path = $_[0];
+  # find all class implementations in this script
+  my $code = read_script($path);
+  foreach my $class ($code =~ m/\bclass\s+(\w+)/g) {
+    $classes{$class} = $path;
+  }
+
+  # find all resources loaded explicitly by this script
+  foreach my $load ($code =~ m/Resources.Load\s*(?:<\s*\w+\s*>\s*)?\(([^)]*)/g) {
+    if ($load =~ m/\"(.*)\"\s*/) {
+      my $loadarg = $1;
+      # check for the only expression we recognize
+      if ($loadarg =~ m/\" \+ sceneName \+ \"/) {
+        foreach my $scene (keys %scenes) {
+	  my $resource = "$assets/Resources/$loadarg";
+	  $resource =~ s/\" \+ sceneName \+ \"/$scene/;
+	  if (-f "$resource.prefab") {
+	    $resources{$path}{$resource} = 1;
+	  }
+	}
+      }
+      else {
+        my $resource = "$assets/Resources/$loadarg";
+        $resources{$path}{$resource} = 1;
+      }
+    }
+    #else {
+    #  print "Unrecognized resource in Resources.Load=$loadarg\n";
+    #}
+  }
+
+  # find all audio clips played by this script
+  # P3SAAudio.Instance.PlaySound("FX/Blackout");
+  # P3SAAudio.Instance.PlaySound3D("PlayerAdded", gameObject.transform);
+  foreach my $play ($code =~ m/PlaySound3?D?\s*\(([^)]*)/g) {
+    if ($play =~ m/^\s*\"([^"]+)\"/) {
+      my $sound = "$assets/Resources/Sound/$1";
+      $resources{$path}{$sound} = 1;
+    }
+  }
+
+  # find all identifiers in the code, some of them might be class names
+  foreach my $identifier ($code =~ m/\w+/g) {
+    $identifiers{$path}{$identifier} = 1;
+  }
+}
+
+sub process_asset {
+  my $path = $_[0];
+  my $asset = read_file($path);
+  foreach my $ref ($asset =~ m/, guid: ([^,]*),/g) {
+    $refs{$path}{$ref} = 1;
+  }
+
+  foreach my $clip ($asset =~ m/^\s*(?:clipName|\w*Clip):\s*(.*)/mg) {
+    if ($clip =~ m/\w/) {
+      my $resource = "$assets/Resources/Sound/$clip";
+      $resources{$path}{$resource} = 1;
+    }
+  }
+}
+
+# find the enabled scenes in the build settings
+# don't use a YAML package, we want to run on bare Git Perl distribution
+
+sub find_scenes {
+  open(my $fh, '<', $buildsettings) or die "Can't open file $buildsettings: $!";
+  my $enabled = "0";
+  
+  while (my $line = <$fh>) {
+    chomp $line;
+    if ($line =~ /- enabled: (\d)/) {
+      $enabled = $1;
+    }
+    elsif ($line =~ m/path: (.*)/) {
+      if ($enabled) {
+        my $path = $1;
+        my $root = "$project/$path";
+        $roots{$root} = 1;
+        $path =~ m/.*\/(\w+)\.unity$/;
+        my $scene = $1;
+        $scenes{$scene} = 1;
+      }
+    }
+  }
+
+  close $fh;
+}
+
+# mark every file under the specified directory (and subdirs) as roots
+sub mark_roots {
+  my $dir = $_[0];
+  my $dirpath = $dir . "/";
+  foreach my $path (keys %guids) {
+    $roots{$path} = 1 if rindex($path, $dirpath, 0) == 0;
+  }
+}
+
+# mark DLLs as used
+sub mark_dlls {
+  if (-f $_ && $_ =~ /\.dll$/) {
+    my $dll = $File::Find::name;
+    $used{$dll} = 1;
+  }
+}
+
+sub find_appcode {
+  my $contents = read_file($appconfig);
+  $contents =~ m/\"Name\": \"(\w+)\"/;
+  $appcode = $1;
+  $appcode || die "Cannot find app code in $appconfig\n";
+  return $appcode;
+}
+
+sub is_binary {
+  my $filename = $_[0];
+  return ($filename =~ /\.(ogg|wav|sfk|mp3|png|zip|ttf|tif|so|dylib|dll|tga|jpg|psd)$/);
+}
+
+# List of extensions in P3SampleApp
+# .physicmaterial
+# .ogg
+# .cubemap
+# .manifest
+# .js
+# .zip
+# .def
+# .ttf
+# .fbx
+# .fontsettings
+# .meta
+# .mixer
+# .html
+# .shader
+# .controller
+# .tif
+# .so
+# .prefab
+# .mat
+# .FBX
+# .wav
+# .dylib
+# .DS_Store
+# .cginc
+# .anim
+# .cs
+# .flare
+# .tga
+# .jpg
+# .png
+# .unity
+# .physicMaterial
+# .mp3
+# .dll
+# .psd
+# .asset
+# .txt
+# .sfk
+
+sub is_used_dir {
+  my $dir = $_[0];
+  my $dirpath = $dir . "/";
+
+  foreach my $path (keys %used) {
+    return 1 if rindex($path, $dirpath, 0) == 0;
+  }
+
+  return 0;
+}
+
+sub read_file {
+  my $path = $_[0];
+  my $content = "";
+  
+  open(my $fh, "<", $path) or die "Could not open file '$path' $!";
+  {
+    local $/;
+    $content = <$fh>;
+  }
+
+  close($fh);
+  return $content;
+}
+
+sub read_script {
+  my $path = $_[0];
+  my $code = read_file($path);
+  $code = remove_comments($code);
+  return $code;
+}
+
+sub remove_comments {
+  my $code = $_[0];
+  # taken from https://metacpan.org/pod/perlfaq6#How-do-I-use-a-regular-expression-to-strip-C-style-comments-from-a-file
+  $code =~ s#/\*[^*]*\*+([^/*][^*]*\*+)*/|//([^\\]|[^\n][\n]?)*?\n|("(\\.|[^"\\])*"|'(\\.|[^'\\])*'|.[^/"'\\]*)#defined $3 ? $3 : ""#gse;
+  return $code;
+}
